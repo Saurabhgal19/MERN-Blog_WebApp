f77514d15846df0346f793aaaee4f743ab5475f8
Saurabh Galpalliwar <121440363+Saurabhgal19@users.noreply.github.com>
2024-01-31 16:46:04 +0530

    create signup backend code and api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,56 @@
+import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+
+export const signup = async (req,res) => {
+try{
+    const { username ,email ,password } = req.body;
+
+    //validation
+    if(!username || 
+        !email || 
+        !password ||
+        username === '' || 
+        email ==='' || 
+        password === ''
+        ) {
+        // validation message
+        return res.status(403).json({
+            success:false,
+            message:'Fill all fields'})
+    }
+
+    //existing user
+    const existingUser = await User.findOne({email});
+    
+        if(existingUser){
+            return res.status(401).json({
+                success:false,
+                message: "Email already exists"
+            })
+    }
+
+    //password hashing
+    const hashedPassword = await bcryptjs.hashSync(password, 10);
+
+    //create entry into DB
+    const newUser = new User({
+        username,
+        email,
+        password:hashedPassword,
+    });
+    //save data into mongo
+    await newUser.save();
+
+    return res.status(200).json({
+        success:true,
+        message:"User is registered Successfully",
+        newUser,
+    });
+}catch (error) {
+        console.error("Error during user registration", error);
+        return res.status(500).json({
+            success:false,
+            message:"User cannot be registrered. Please try again"})
+            //message:  error.message,
+   } 
+};
