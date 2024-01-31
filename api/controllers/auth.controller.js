6bd0728129e1f4d79b02251a3d5a95ead3ebaa56
Saurabh Galpalliwar <121440363+Saurabhgal19@users.noreply.github.com>
2024-01-31 17:11:57 +0530

    add middleware function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,22 +1,26 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) => {
+export const signup = async (req,res,next) => {
 try{
     const { username ,email ,password } = req.body;
 
     //validation
-    if(!username || 
+    if( !username || 
         !email || 
         !password ||
         username === '' || 
         email ==='' || 
         password === ''
         ) {
+
+        next(errorHandler(400, "All fields are required"));
+        
         // validation message
-        return res.status(403).json({
-            success:false,
-            message:'Fill all fields'})
+        // return res.status(403).json({
+        //     success:false,
+        //     message:'Fill all fields'})
     }
 
     //existing user
@@ -46,11 +50,15 @@ try{
         message:"User is registered Successfully",
         newUser,
     });
-}catch (error) {
+}
+    catch (error) {
+
         console.error("Error during user registration", error);
-        return res.status(500).json({
-            success:false,
-            message:"User cannot be registrered. Please try again"})
-            //message:  error.message,
+        next(error);
+        // return res.status(500).json({
+        //     success:false,
+        //     message:"User cannot be registrered. Please try again",
+        //     //message:  error.message,    
+        // })    
    } 
 };
