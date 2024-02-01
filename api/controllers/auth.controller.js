df403dc1338ba7827d452799804ec470a8fd9bf6
Saurabh Galpalliwar <121440363+Saurabhgal19@users.noreply.github.com>
2024-02-01 19:37:31 +0530

    Adding SignIn Funtionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req,res,next) => {
 try{
@@ -62,3 +63,50 @@ try{
         // })    
    } 
 };
+
+//Sign In Code
+
+export const signin = async (req,res,next) => {
+
+    //get data
+    const{email, password}= req.body;
+
+    if(!email || !password || email ==="" || password ===""){
+       return next(errorHandler(400, 'All fields are required'))
+    }
+
+    try{
+        const validUser = await User.findOne({email})
+
+        if(!validUser){
+            return next(errorHandler(404,'User not found'));
+        }
+        //comparing password
+        const validPassword= bcryptjs.compareSync(password, validUser.password);
+
+        if(!validPassword){
+            return next(errorHandler(401,"Invalid Password"));
+        }
+
+        //create token
+        const token = jwt.sign({ id: validUser._id,},
+            process.env.JWT_SECRET,
+            {expiresIn:'1d'}
+        )
+
+        //hide a password from userData
+        const {password : pass,  ...rest} = validUser._doc
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly:true}).json({
+                success: true,
+                token: token,
+                message:"SignIn Successfully",
+                //validUser,
+                rest,
+            });
+    }
+    catch(error){
+        return next(errorHandler(500,"Invalid Response"));
+    }
+}
